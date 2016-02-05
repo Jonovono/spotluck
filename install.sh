38523febe6925358ebe31561f4ea2a16cb2ee79a
Jordan Howlett <jordan@howlett.io>
2016-02-04 22:35:50 -0500

    try this node installer

--- install.sh
+++ install.sh
@@ -6,5 +6,15 @@ exists()
 if exists node; then
   echo "Node exists"
 else
-  curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+  # curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+  echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+  . ~/.bashrc
+  mkdir ~/local
+  mkdir ~/node-latest-install
+  cd ~/node-latest-install
+  curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+  ./configure --prefix=~/local
+  make install # ok, fine, this step probably takes more than 30 seconds...
+  curl https://www.npmjs.org/install.sh | sh
 fi
