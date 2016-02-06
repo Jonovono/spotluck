8fad90eae9cec9d2196c83f0a7dfccd452162907
Jordan Howlett <jordan@howlett.io>
2016-02-06 14:50:20 -0500

    refactor so nice

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-var HOST = 'mqtt://pijxlqsf:gA-ARJgAyGHt@m10.cloudmqtt.com:10089'
+// var HOST = 'mqtt://pijxlqsf:gA-ARJgAyGHt@m10.cloudmqtt.com:10089'
+var HOST = 'http://162.243.102.150:8000/faye';
 module.exports.HOST = HOST;
