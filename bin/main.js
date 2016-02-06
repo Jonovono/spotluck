352dbc8474ee13042c01bbe2d4871c6b0b574d42
Jordan Howlett <jordan@howlett.io>
2016-02-06 10:37:28 -0500

    refactoring

--- bin/main.js
+++ bin/main.js
@@ -1,22 +1,13 @@
 #! /usr/bin/env node
 
-var pub = require("../lib/pub")
-var sub = require("../lib/sub")
-var player = require('../lib/player');
+var spotify = require("spotify-node-applescript");
+var CLI = require('../lib/cli');
 
-player.spotifyRunning()
+spotify.isRunning(function(err, isRunning) {
+  if (!isRunning) {
+    winston.error('Please start Spotify')
+    process.exit()
+  }
 
-var args = process.argv.slice(2)
-
-var command = args[0]
-var channel_id = args[1]
-
-if (command !== 'start' || command !== 'play') {
-  console.log('Enter a command')
-}
-
-if (command === 'play') {
-  sub.start(channel_id)
-} else if (command === 'start') {
-    pub.start()
-}
+  new CLI()
+});
