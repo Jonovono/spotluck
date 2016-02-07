b4862753962ceabf1688aef3ed149423856ae714
Jordan Howlett <jordan@howlett.io>
2016-02-07 13:04:38 -0500

    update config endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // var HOST = 'mqtt://pijxlqsf:gA-ARJgAyGHt@m10.cloudmqtt.com:10089'
-var HOST = 'http://162.243.102.150:8000/faye';
+var HOST = 'http://162.243.108.221:4002/faye';
 module.exports.HOST = HOST;
